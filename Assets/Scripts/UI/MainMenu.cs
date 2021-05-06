0de0eb6f7ac4d1375fc04ecbbb2d77745d8b46b5
Laura <laura.luisa.schmid@gmail.com>
2021-05-06 10:53:09 +0200

    animated menu without levelselect

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,18 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public Animator playTransition;
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadGame());
     }
 
     public void QuitGame()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    IEnumerator LoadGame()
+    {
+        playTransition.SetTrigger("StartGame");
+
+        yield return new WaitForSeconds(1.75f);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
